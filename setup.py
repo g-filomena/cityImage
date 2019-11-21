54f478c7b03096fab7c3c9263346af9783568aa0
gabrif7 <gabriele.filomena@uni-muenster.de>
2019-11-21 18:37:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ with open("README.md", "r") as readme_file:
 requirements = ["osmnx>=0.10", "seaborn>=0.9.0"]
 setup(
     name='urbanFormPy',
-    packages=['urbanFormPy'],
     description='A package for studying urban Form and obtaining the computational Image of the City',
     long_description=readme,
     long_description_content_type="text/markdown",
