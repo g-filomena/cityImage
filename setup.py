0ffcc94e1d8e2bee25730452a102d9a4db99f948
gabrif7 <gabriele.filomena@uni-muenster.de>
2019-11-21 18:36:22 +0100

    setup updates

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["osmnx>=0.10", "seaborn>=0.9.0"]
 setup(
     name='urbanFormPy',
     packages=['urbanFormPy'],
     description='A package for studying urban Form and obtaining the computational Image of the City',
-    version='0.1',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+	packages=find_packages(),
+	version='0.1',
     url='http://github.com/urbanFormPy',
     author='Gabriele Filomena',
     author_email='gabriele.filomena@uni-muenster.de',
-    keywords=['pip','urban Form Analysis','Computational Image of the City']
+	install_requires=requirements,
+    keywords=['pip','urban Form Analysis','Computational Image of the City'],
     )
