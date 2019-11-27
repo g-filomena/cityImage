208064ed8d939a95a21c1f15035796c42ad7009a
gabrif7 <gabriele.filomena@uni-muenster.de>
2019-11-27 15:15:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(
     description='A package for studying urban Form and obtaining the computational Image of the City',
     long_description = readme,
     long_description_content_type="text/markdown",
-	packages=find_packages(),
+	packages=find_packages('urbanFormPy'),  # include all packages under src
+    package_dir={'':'urbanFormPy'},   # tell distutils packages are under src
 	version='0.1',
     author='Gabriele Filomena',
     author_email='gabriele.filomena@uni-muenster.de',
