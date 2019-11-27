ae2db9e3c374961a3b3abc2df0096f1576ca9cc3
gabrif7 <gabriele.filomena@uni-muenster.de>
2019-11-27 15:13:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as readme_file: readme = readme_file.read()
 
@@ -10,7 +10,7 @@ setup(
     description='A package for studying urban Form and obtaining the computational Image of the City',
     long_description = readme,
     long_description_content_type="text/markdown",
-	packages = ['urbanFormPy'],
+	packages=find_packages(),
 	version='0.1',
     author='Gabriele Filomena',
     author_email='gabriele.filomena@uni-muenster.de',
