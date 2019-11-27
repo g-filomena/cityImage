3ca81ce05770188e14760ed1bfcead2a113b35cb
gabrif7 <gabriele.filomena@uni-muenster.de>
2019-11-27 13:52:51 +0100

    fix

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
 from setuptools import setup
 
-# with open("README.md", "r") as readme_file:
-    # readme = readme_file.read()
+with open("README.md", "r") as readme_file: readme = readme_file.read()
 
-# requirements = ["osmnx>=0.10", "seaborn>=0.9.0",  "descartes>=1.1", "matplotlib>=3.1",
-    # "networkx>=2.4", "osmnx>=0.10", "pysal>=2.1", "python-louvain>=0.13"]
+requirements = ["osmnx>=0.10", "seaborn>=0.9.0",  "descartes>=1.1", "matplotlib>=3.1",
+    "networkx>=2.4", "osmnx>=0.10", "pysal>=2.1", "python-louvain>=0.13"]
 	
 setup(
     name='urbanFormPy',
     description='A package for studying urban Form and obtaining the computational Image of the City',
-    long_description=readme,
+    long_description = readme,
     long_description_content_type="text/markdown",
 	packages = ['urbanFormPy'],
 	version='0.1',
-    url='http://github.com/urbanFormPy',
     author='Gabriele Filomena',
     author_email='gabriele.filomena@uni-muenster.de',
 	install_requires=requirements,
