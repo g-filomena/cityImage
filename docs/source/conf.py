e27a10fda5537402621ad5f517ac6589dc016091
Gabriele Filomena <gabriele.filomena@uni-muenster.de>
2021-02-07 20:19:12 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ autodoc_mock_imports = [
     "geopandas", 
     "pandas",
     "networkx"
-    "community"
+    "community",
     "osmnx",
 ]
 
@@ -43,7 +43,7 @@ release = '0.14'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+extensions = ["sphinx.ext.faculty"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
