cf4374670e3e37a608ebb5ba5fb07c2a9c2a5688
Gabriele Filomena <gabriele.filomena@uni-muenster.de>
2021-02-12 20:03:28 +0100

    Update load.py

--- cityImage/load.py
+++ cityImage/load.py
@@ -88,6 +88,7 @@ def get_network_fromOSM(place, download_method, network_type = "all", epsg = Non
     if epsg is None: 
         nodes_gdf, edges_gdf = ox.projection.project_gdf(nodes_gdf), ox.projection.project_gdf(edges_gdf)
     else: 
+        crs = 'EPSG:' + str(epsg)
         nodes_gdf, edges_gdf = nodes_gdf.to_crs(crs), edges_gdf.to_crs(crs)
     
     nodes_gdf["x"], nodes_gdf["y"] = list(zip(*[(r.coords[0][0], r.coords[0][1]) for r in nodes_gdf.geometry]))
