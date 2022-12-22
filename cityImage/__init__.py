65316ac4f2d92619d5d38906016e1bd5b507a364
Yuhao Kang <kkyyhh96@163.com>
2022-12-22 17:13:39 -0600

    Update __init__.py
    plot package should be removed.

--- cityImage/__init__.py
+++ cityImage/__init__.py
@@ -13,7 +13,7 @@ from .clean import *
 from .graph import *
 from .landmarks import *
 from .land_use import*
-from .plot import *
+# from .plot import *
 from .regions import *
 from .utilities import *
 from .simplify_junctions import *
