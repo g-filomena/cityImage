29de0340fb7c6bb4e6e8362a19f6bde32106ac88
Gabriele Filomena <gabriele.filomena@uni-muenster.de>
2023-01-24 18:42:24 +0100

    Update test_cityImage.py

--- tests/test_cityImage.py
+++ tests/test_cityImage.py
@@ -9,6 +9,7 @@ import geopandas as gpd
 
 from shapely.geometry import LineString, MultiLineString, MultiPoint, MultiPolygon, Point, Polygon
 import cityImage as ci
+from matplotlib.colors import LinearSegmentedColormap
 
 # define queries to use throughout tests
 
@@ -22,9 +23,9 @@ distance = 1500
 location = (45.1383, 7.0509)
 nodes_gdf, edges_gdf = None, None
 nodes_gdf_susa, edges_gdf_susa = None, None
+barriers_gdf = None
 graph = None 
 
-# Test load.py
 def test_loadOSM():
     global nodes_gdf_susa
     global edges_gdf_susa
@@ -40,14 +41,13 @@ def test_loadSHP_topology():
     global nodes_gdf
     global edges_gdf
     global epsg_york
-    input_path = '/tests/input/York_street_network.shp'
+    input_path = 'tests/input/York_street_network.shp'
     dict_columns = {"roadType_field": "type",  "direction_field": "oneway", "speed_field": "maxspeed", "name_field": "name"}    
     nodes_gdf, edges_gdf = ci.get_network_fromSHP(input_path, epsg_york, dict_columns = dict_columns, other_columns = [])
     # fix topology
     nodes_gdf, edges_gdf = ci.clean_network(nodes_gdf, edges_gdf, dead_ends = True, remove_islands = True, 
                                             same_uv_edges = True, self_loops = True, fix_topology = False)
     
-# Test graph.py
 def test_graph():
     global nodes_gdf
     global edges_gdf
@@ -59,7 +59,6 @@ def test_graph():
     nodes_dual, edges_dual = ci.dual_gdf(nodes_gdf, edges_gdf, epsg = epsg_york, oneway = False, angle = 'degree')
     dual_graph = ci.dual_graph_fromGDF(nodes_dual, edges_dual)
 
-# Test angles.py
 def test_angles():
     global edges_gdf
 
@@ -72,7 +71,6 @@ def test_angles():
     angle_deflection = ci.angle_line_geometries(line_geometryA, line_geometryB, degree = True, calculation_type = 'deflection')
     angle_angular = ci.angle_line_geometries(line_geometryA, line_geometryB, degree = True, calculation_type = 'angular_change')
     
-# # Test centrality.py
 def test_centrality():
     global graph
     global nodes_gdf
@@ -99,34 +97,6 @@ def test_centrality():
     Eb = nx.edge_betweenness_centrality(graph, weight = weight, normalized = False)
     edges_gdf = ci.append_edges_metrics(edges_gdf, graph, [Eb], ['Eb'])
     
-def test_plot():
-    global nodes_gdf
-    global edges_gdf
-    global edges_gdf_susa
-    
-    tmp_nodes = nodes_gdf.copy()
-    base_map_dict = {'base_map_gdf': edges_gdf, 'base_map_alpha' : 0.4, 'base_map_geometry_size' : 1.1, 'base_map_zorder' : 0}
-    # Lynch's bins - only for variables from 0 to 1    
-        
-
-    ci.scaling_columnDF(tmp_nodes, 'Bc')
-    scheme_dict = {'column' : "Bc_sc", 'bins' : [0.125, 0.25, 0.5, 0.75, 1.00], 'scheme' : 'User_Defined'}
-    cbar_dict = {'cbar' : True, 'cbar_ticks' : 2, 'cbar_max_symbol' : True, 'cbar_min_max' : True}
-    cmap = ci.kindlmann()
-    plot = ci.plot_gdf(gdf = tmp_nodes, black_background = True, cmap = cmap, legend = True, axes_frame = True, 
-                       geometry_size = 25, **base_map_dict, **scheme_dict) 
-    
-    columns = ['Bc', 'Sc', 'Cc', 'Ic']
-    cmap = ci.cmap_from_colors(['yellow', 'orange', 'red'])
-    plot = ci.plot_gdf_grid(gdf = tmp_nodes, columns = columns, black_background = True, cmap = cmap, legend = False, 
-                           geometry_size_factor = 30, **cbar_dict) 
-    
-    cmap = ci.cmap_from_colors(['red', 'blue'])
-    plot_edges = ci.plot_gdf(edges_gdf, column = 'Eb', black_background = True, scheme = 'Fisher_Jenks', cmap = cmap, 
-                             norm = None, legend = False, **cbar_dict)
-    plot_multi = ci.plot_gdfs([edges_gdf, edges_gdf_susa], column = "length", black_background = True, scheme = 'Fisher_Jenks', 
-                              cmap = cmap, **cbar_dict)
-   
 def test_regions():
     global nodes_gdf_susa
     global edges_gdf_susa
@@ -149,21 +119,55 @@ def test_regions():
     nodes_gdf_susa = ci.amend_nodes_membership(nodes_gdf_susa, edges_gdf_susa, 'p_topo', min_size_district = min_size_district)
     nodes_gdf_susa = ci.find_gateways(nodes_gdf_susa, edges_gdf_susa, 'p_topo')
    
-# # Test barriers.py
-# def test_barriers(): 
-
-    # barriers_gdf = ci.get_barriers(place, download_method, epsg = epsg)
-
-    # # assign barriers to street network
-    # edges_gdf_updated = ci.along_within_parks(edges_gdf, barriers_gdf)
-    # edges_gdf_updated_sindex = edges_gdf_updated.sindex
-    # edges_gdf_updated = ci.along_water(edges_gdf, barriers_gdf)
-    # edges_gdf_updated = ci.along_within_parks(edges_gdf, barriers_gdf)
-    # edges_gdf_updated = ci.assign_structuring_barriers(edges_gdf_updated, barriers_gdf)
+def test_barriers(): 
+    global barriers_gdf
+    global place
+    global epsg_susa
     
-    # base_map_dict = {'gdf_base_map': edges_gdf, 'base_map_alpha' : 0.4, 'base_map_lw' : 1.1, 'base_map_zorder' : 0}
-    # plot = ci.plot_barriers(barriers_gdf, lw = 1.1, legend = True, axis_frame = False, black_background = True, fig_size = 15, **base_map_dict)
+    barriers_gdf = ci.get_barriers(place, download_method, epsg = epsg_susa)
+    # assign barriers to street network
+    edges_gdf_updated = ci.along_within_parks(edges_gdf_susa, barriers_gdf)
+    edges_gdf_updated_sindex = edges_gdf_updated.sindex
+    edges_gdf_updated = ci.along_water(edges_gdf_susa, barriers_gdf)
+    edges_gdf_updated = ci.along_within_parks(edges_gdf_updated, barriers_gdf)
+    edges_gdf_updated = ci.assign_structuring_barriers(edges_gdf_updated, barriers_gdf)
     
+def test_plot():
+    global nodes_gdf
+    global edges_gdf
+    global edges_gdf_susa
+    global barriers_gdf
+    tmp_nodes = nodes_gdf.copy()
+    base_map_dict = {'base_map_gdf': edges_gdf, 'base_map_alpha' : 0.4, 'base_map_geometry_size' : 1.1, 'base_map_zorder' : 0}
+    # Lynch's bins - only for variables from 0 to 1    
+
+    ci.scaling_columnDF(tmp_nodes, 'Bc')
+    scheme_dict = {'column' : "Bc_sc", 'bins' : [0.125, 0.25, 0.5, 0.75, 1.00], 'scheme' : 'User_Defined'}
+    cbar_dict = {'cbar' : True, 'cbar_ticks' : 2, 'cbar_max_symbol' : True, 'cbar_min_max' : True}
+    cmap = ci.kindlmann()
+    plot = ci.plot_gdf(gdf = tmp_nodes, black_background = True, cmap = cmap, legend = True, axes_frame = True, 
+                       geometry_size = 25, **base_map_dict, **scheme_dict) 
+    
+    columns = ['Bc', 'Sc', 'Cc', 'Ic']
+    cmap = ci.cmap_from_colors(['yellow', 'orange', 'red'])
+    plot = ci.plot_gdf_grid(gdf = tmp_nodes, columns = columns, black_background = True, cmap = cmap, legend = False, 
+                           geometry_size_factor = 30, **cbar_dict) 
+    
+    cmap = ci.cmap_from_colors(['red', 'blue'])
+    plot_edges = ci.plot_gdf(edges_gdf, column = 'Eb', black_background = True, scheme = 'Fisher_Jenks', cmap = cmap, 
+                             norm = None, legend = False, **cbar_dict)
+    plot_multi = ci.plot_gdfs([edges_gdf, edges_gdf_susa], column = "length", black_background = True, scheme = 'Fisher_Jenks', 
+                              cmap = cmap, **cbar_dict)   
+                              
+    base_map_dict = {'gdf_base_map': edges_gdf_susa, 'base_map_alpha' : 0.4, 'base_map_geometry_size' : 1.1, 'base_map_zorder' : 0}
+
+    barriers_gdf.sort_values(by = 'barrier_type', ascending = False, inplace = True)  
+    colors = ['green', 'red', 'gray', 'blue']
+    if 'secondary_road' in list(barriers_gdf['barrier_type'].unique()):
+        colors.append('gold')
+    cmap = LinearSegmentedColormap.from_list('new_map', colors, N=len(colors)) 
+    plot = ci.plot_gdf(barriers_gdf, column = 'barrier_type', geometry_size = 1.1, legend = True, axes_frame = False, black_background = False, cmap = cmap)                          
+                              
 # def test_landmarks():
     
     # epsg = 3003
