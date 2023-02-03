05d4062ab70a792fc2ddde587c91804a31d93307
Gabriele Filomena <gabriele.filomena@uni-muenster.de>
2023-02-03 10:28:49 +0100

    Update test_cityImage.py

--- tests/test_cityImage.py
+++ tests/test_cityImage.py
@@ -37,174 +37,188 @@ def test_loadOSM():
     _, _ = ci.get_network_fromOSM(address, 'distance_from_address', network_type = "all", 
                                                           epsg = epsg_susa, distance = distance)
     
-def test_loadSHP_topology():  
-    global nodes_gdf
-    global edges_gdf
-    global epsg_york
-    input_path = 'tests/input/York_street_network.shp'
-    dict_columns = {"roadType_field": "type",  "direction_field": "oneway", "speed_field": "maxspeed", "name_field": "name"}    
-    nodes_gdf, edges_gdf = ci.get_network_fromSHP(input_path, epsg_york, dict_columns = dict_columns, other_columns = [])
-    # fix topology
-    nodes_gdf, edges_gdf = ci.clean_network(nodes_gdf, edges_gdf, dead_ends = True, remove_islands = True, 
-                                            same_uv_edges = True, self_loops = True, fix_topology = False)
+# def test_loadSHP_topology():  
+    # global nodes_gdf
+    # global edges_gdf
+    # global epsg_york
+    # input_path = 'tests/input/York_street_network.shp'
+    # dict_columns = {"roadType_field": "type",  "direction_field": "oneway", "speed_field": "maxspeed", "name_field": "name"}    
+    # nodes_gdf, edges_gdf = ci.get_network_fromSHP(input_path, epsg_york, dict_columns = dict_columns, other_columns = [])
+    # # fix topology
+    # nodes_gdf, edges_gdf = ci.clean_network(nodes_gdf, edges_gdf, dead_ends = True, remove_islands = True, 
+                                            # same_uv_edges = True, self_loops = True, fix_topology = False)
     
-def test_graph():
-    global nodes_gdf
-    global edges_gdf
-    global graph
-    global epsg_york
+# def test_graph():
+    # global nodes_gdf
+    # global edges_gdf
+    # global graph
+    # global epsg_york
     
-    graph = ci.graph_fromGDF(nodes_gdf, edges_gdf, nodeID = 'nodeID')
-    multi_graph_fromGDF = ci.multiGraph_fromGDF(nodes_gdf, edges_gdf, 'nodeID')
-    nodes_dual, edges_dual = ci.dual_gdf(nodes_gdf, edges_gdf, epsg = epsg_york, oneway = False, angle = 'degree')
-    dual_graph = ci.dual_graph_fromGDF(nodes_dual, edges_dual)
+    # graph = ci.graph_fromGDF(nodes_gdf, edges_gdf, nodeID = 'nodeID')
+    # multi_graph_fromGDF = ci.multiGraph_fromGDF(nodes_gdf, edges_gdf, 'nodeID')
+    # nodes_dual, edges_dual = ci.dual_gdf(nodes_gdf, edges_gdf, epsg = epsg_york, oneway = False, angle = 'degree')
+    # dual_graph = ci.dual_graph_fromGDF(nodes_dual, edges_dual)
 
-def test_angles():
-    global edges_gdf
+# def test_angles():
+    # global edges_gdf
 
-    nodeID = edges_gdf.iloc[0].u
-    tmp = edges_gdf[(edges_gdf.u == nodeID) | (edges_gdf.v == nodeID)].copy()
-    line_geometryA = tmp.iloc[0].geometry
-    line_geometryB = tmp.iloc[1].geometry
+    # nodeID = edges_gdf.iloc[0].u
+    # tmp = edges_gdf[(edges_gdf.u == nodeID) | (edges_gdf.v == nodeID)].copy()
+    # line_geometryA = tmp.iloc[0].geometry
+    # line_geometryB = tmp.iloc[1].geometry
     
-    angle = ci.angle_line_geometries(line_geometryA, line_geometryB, degree = True, calculation_type = 'vectors')
-    angle_deflection = ci.angle_line_geometries(line_geometryA, line_geometryB, degree = True, calculation_type = 'deflection')
-    angle_angular = ci.angle_line_geometries(line_geometryA, line_geometryB, degree = True, calculation_type = 'angular_change')
+    # angle = ci.angle_line_geometries(line_geometryA, line_geometryB, degree = True, calculation_type = 'vectors')
+    # angle_deflection = ci.angle_line_geometries(line_geometryA, line_geometryB, degree = True, calculation_type = 'deflection')
+    # angle_angular = ci.angle_line_geometries(line_geometryA, line_geometryB, degree = True, calculation_type = 'angular_change')
     
-def test_centrality():
-    global graph
-    global nodes_gdf
-    global edges_gdf
-    global epsg_york
-    weight = 'length'
-    services = ox.geometries_from_address(address, tags = {'amenity':True}, dist = distance).to_crs(epsg=epsg_york)
-    services = services[services['geometry'].geom_type == 'Point']
-    graph = ci.weight_nodes(nodes_gdf, services, graph, field_name = 'services', radius = 50)
+# def test_centrality():
+    # global graph
+    # global nodes_gdf
+    # global edges_gdf
+    # global epsg_york
+    # weight = 'length'
+    # services = ox.geometries_from_address(address, tags = {'amenity':True}, dist = distance).to_crs(epsg=epsg_york)
+    # services = services[services['geometry'].geom_type == 'Point']
+    # graph = ci.weight_nodes(nodes_gdf, services, graph, field_name = 'services', radius = 50)
 
-    measure = 'betweenness_centrality'
-    Rc = ci.reach_centrality(graph,  weight = weight, radius = 400, attribute = 'services')
-    Bc = ci.centrality(graph, nodes_gdf, measure = 'betweenness_centrality', weight = weight, normalized = False)
-    Sc = ci.centrality(graph, nodes_gdf, measure = 'straightness_centrality', weight = weight, normalized = False)
-    Cc = ci.centrality(graph, nodes_gdf, measure = 'closeness_centrality', weight = weight, normalized = False)
-    Ic = ci.centrality(graph, nodes_gdf, measure = 'information_centrality', weight = weight, normalized = False)
+    # measure = 'betweenness_centrality'
+    # Rc = ci.reach_centrality(graph,  weight = weight, radius = 400, attribute = 'services')
+    # Bc = ci.centrality(graph, nodes_gdf, measure = 'betweenness_centrality', weight = weight, normalized = False)
+    # Sc = ci.centrality(graph, nodes_gdf, measure = 'straightness_centrality', weight = weight, normalized = False)
+    # Cc = ci.centrality(graph, nodes_gdf, measure = 'closeness_centrality', weight = weight, normalized = False)
+    # Ic = ci.centrality(graph, nodes_gdf, measure = 'information_centrality', weight = weight, normalized = False)
     
-    # Appending the attributes to the geodataframe
-    dicts = [Bc, Sc, Cc, Ic]
-    columns = ['Bc', 'Sc', 'Cc', 'Ic']
-    for n, c in enumerate(dicts): 
-        nodes_gdf[columns[n]] = nodes_gdf.nodeID.map(c)
+    # # Appending the attributes to the geodataframe
+    # dicts = [Bc, Sc, Cc, Ic]
+    # columns = ['Bc', 'Sc', 'Cc', 'Ic']
+    # for n, c in enumerate(dicts): 
+        # nodes_gdf[columns[n]] = nodes_gdf.nodeID.map(c)
         
-    Eb = nx.edge_betweenness_centrality(graph, weight = weight, normalized = False)
-    edges_gdf = ci.append_edges_metrics(edges_gdf, graph, [Eb], ['Eb'])
+    # Eb = nx.edge_betweenness_centrality(graph, weight = weight, normalized = False)
+    # edges_gdf = ci.append_edges_metrics(edges_gdf, graph, [Eb], ['Eb'])
     
-def test_regions():
+# def test_regions():
+    # global nodes_gdf_susa
+    # global edges_gdf_susa
+    # global epsg_susa
+    
+    # graph_susa = ci.graph_fromGDF(nodes_gdf_susa, edges_gdf_susa, nodeID = 'nodeID')
+    # nodes_dual, edges_dual = ci.dual_gdf(nodes_gdf_susa, edges_gdf_susa, epsg = epsg_susa, oneway = False, angle = 'degree')
+    # dual_graph = ci.dual_graph_fromGDF(nodes_dual, edges_dual)
+    
+    # dual_regions = ci.identify_regions(dual_graph, edges_gdf_susa, weight = None)
+    # primal_regions = ci.identify_regions_primal(graph_susa, nodes_gdf_susa, weight = None)
+    
+    # polygons_gdf = ci.polygonise_partitions(dual_regions, 'p_topo', convex_hull = False, buffer = 30)
+    # polygons_gdf = ci.polygonise_partitions(dual_regions, 'p_topo', convex_hull = True, buffer = 30)
+    # edges_updated = ci.districts_to_edges_from_nodes(primal_regions, edges_gdf_susa, 'p_topo')
+    # nodes_updated = ci.district_to_nodes_from_edges(nodes_gdf_susa, dual_regions, 'p_topo')
+    
+    # nodes_gdf_susa = ci.district_to_nodes_from_polygons(nodes_gdf_susa, polygons_gdf, 'p_topo')
+    # min_size_district = 10
+    # nodes_gdf_susa = ci.amend_nodes_membership(nodes_gdf_susa, edges_gdf_susa, 'p_topo', min_size_district = min_size_district)
+    # nodes_gdf_susa = ci.find_gateways(nodes_gdf_susa, edges_gdf_susa, 'p_topo')
+   
+# def test_barriers(): 
+    # global barriers_gdf
+    # global place
+    # global epsg_susa
+    
+    # barriers_gdf = ci.get_barriers(place, download_method, epsg = epsg_susa, parks_min_area = 200)
+    # # assign barriers to street network
+    # edges_gdf_updated = ci.along_within_parks(edges_gdf_susa, barriers_gdf)
+    # edges_gdf_updated = ci.along_water(edges_gdf_susa, barriers_gdf)
+    # edges_gdf_updated = ci.along_within_parks(edges_gdf_updated, barriers_gdf)
+    # edges_gdf_updated = ci.assign_structuring_barriers(edges_gdf_updated, barriers_gdf)
+    
+# def test_plot():
+    # global nodes_gdf
+    # global edges_gdf
+    # global edges_gdf_susa
+    # global barriers_gdf
+    # tmp_nodes = nodes_gdf.copy()
+    # base_map_dict = {'base_map_gdf': edges_gdf, 'base_map_alpha' : 0.4, 'base_map_geometry_size' : 1.1, 'base_map_zorder' : 0}
+    # # Lynch's bins - only for variables from 0 to 1    
+
+    # ci.scaling_columnDF(tmp_nodes, 'Bc')
+    # scheme_dict = {'column' : "Bc_sc", 'bins' : [0.125, 0.25, 0.5, 0.75, 1.00], 'scheme' : 'User_Defined'}
+    # cbar_dict = {'cbar' : True, 'cbar_ticks' : 2, 'cbar_max_symbol' : True, 'cbar_min_max' : True}
+    # cmap = ci.kindlmann()
+    # plot = ci.plot_gdf(gdf = tmp_nodes, black_background = True, cmap = cmap, legend = True, axes_frame = True, 
+                       # geometry_size = 25, **base_map_dict, **scheme_dict) 
+    
+    # columns = ['Bc', 'Sc', 'Cc', 'Ic']
+    # cmap = ci.cmap_from_colors(['yellow', 'orange', 'red'])
+    # plot = ci.plot_gdf_grid(gdf = tmp_nodes, columns = columns, black_background = True, cmap = cmap, legend = False, 
+                           # geometry_size_factor = 30, **cbar_dict) 
+    
+    # cmap = ci.cmap_from_colors(['red', 'blue'])
+    # plot_edges = ci.plot_gdf(edges_gdf, column = 'Eb', black_background = True, scheme = 'Fisher_Jenks', cmap = cmap, 
+                             # norm = None, legend = False, **cbar_dict)
+    # plot_multi = ci.plot_gdfs([edges_gdf, edges_gdf_susa], column = "length", black_background = True, scheme = 'Fisher_Jenks', 
+                              # cmap = cmap, **cbar_dict)   
+                              
+    # base_map_dict = {'gdf_base_map': edges_gdf_susa, 'base_map_alpha' : 0.4, 'base_map_geometry_size' : 1.1, 'base_map_zorder' : 0}
+
+    # barriers_gdf.sort_values(by = 'barrier_type', ascending = False, inplace = True)  
+    # colors = ['green', 'red', 'gray', 'blue']
+    # if 'secondary_road' in list(barriers_gdf['barrier_type'].unique()):
+        # colors.append('gold')
+    # cmap = LinearSegmentedColormap.from_list('new_map', colors, N=len(colors)) 
+    # plot = ci.plot_gdf(barriers_gdf, column = 'barrier_type', geometry_size = 1.1, legend = True, axes_frame = False, black_background = False, cmap = cmap)                          
+     
+    # cmap = ci.random_colors_list_rgb(nlabels = len(buildings_gdf.land_use.unique()))
+    # plot_buildings = ci.plot_gdf(buildings_gdf, column = 'land_use', black_background = True, legend = True)
+    # plot_flat = ci.plot_gdf(buildings_gdf)
+
+
+     
+def test_landmarks():
+    
+    global epsg_susa
     global nodes_gdf_susa
     global edges_gdf_susa
-    global epsg_susa
     
-    graph_susa = ci.graph_fromGDF(nodes_gdf_susa, edges_gdf_susa, nodeID = 'nodeID')
-    nodes_dual, edges_dual = ci.dual_gdf(nodes_gdf_susa, edges_gdf_susa, epsg = epsg_susa, oneway = False, angle = 'degree')
-    dual_graph = ci.dual_graph_fromGDF(nodes_dual, edges_dual)
-    
-    dual_regions = ci.identify_regions(dual_graph, edges_gdf_susa, weight = None)
-    primal_regions = ci.identify_regions_primal(graph_susa, nodes_gdf_susa, weight = None)
-    
-    polygons_gdf = ci.polygonise_partitions(dual_regions, 'p_topo', convex_hull = False, buffer = 30)
-    polygons_gdf = ci.polygonise_partitions(dual_regions, 'p_topo', convex_hull = True, buffer = 30)
-    edges_updated = ci.districts_to_edges_from_nodes(primal_regions, edges_gdf_susa, 'p_topo')
-    nodes_updated = ci.district_to_nodes_from_edges(nodes_gdf_susa, dual_regions, 'p_topo')
-    
-    nodes_gdf_susa = ci.district_to_nodes_from_polygons(nodes_gdf_susa, polygons_gdf, 'p_topo')
-    min_size_district = 10
-    nodes_gdf_susa = ci.amend_nodes_membership(nodes_gdf_susa, edges_gdf_susa, 'p_topo', min_size_district = min_size_district)
-    nodes_gdf_susa = ci.find_gateways(nodes_gdf_susa, edges_gdf_susa, 'p_topo')
-   
-def test_barriers(): 
-    global barriers_gdf
-    global place
-    global epsg_susa
-    
-    barriers_gdf = ci.get_barriers(place, download_method, epsg = epsg_susa, parks_min_area = 200)
-    # assign barriers to street network
-    edges_gdf_updated = ci.along_within_parks(edges_gdf_susa, barriers_gdf)
-    edges_gdf_updated = ci.along_water(edges_gdf_susa, barriers_gdf)
-    edges_gdf_updated = ci.along_within_parks(edges_gdf_updated, barriers_gdf)
-    edges_gdf_updated = ci.assign_structuring_barriers(edges_gdf_updated, barriers_gdf)
-    
-def test_plot():
-    global nodes_gdf
-    global edges_gdf
-    global edges_gdf_susa
-    global barriers_gdf
-    tmp_nodes = nodes_gdf.copy()
-    base_map_dict = {'base_map_gdf': edges_gdf, 'base_map_alpha' : 0.4, 'base_map_geometry_size' : 1.1, 'base_map_zorder' : 0}
-    # Lynch's bins - only for variables from 0 to 1    
+    # weights
+    g_cW = {'vScore': 0.50, 'sScore' : 0.30, 'cScore': 0.20, 'pScore': 0.10}
+    g_iW = {'3dvis': 0.50, 'fac': 0.30, 'height': 0.20, 'area': 0.30, '2dvis':0.30, 'neigh': 0.20 , 'road': 0.20}
 
-    ci.scaling_columnDF(tmp_nodes, 'Bc')
-    scheme_dict = {'column' : "Bc_sc", 'bins' : [0.125, 0.25, 0.5, 0.75, 1.00], 'scheme' : 'User_Defined'}
-    cbar_dict = {'cbar' : True, 'cbar_ticks' : 2, 'cbar_max_symbol' : True, 'cbar_min_max' : True}
-    cmap = ci.kindlmann()
-    plot = ci.plot_gdf(gdf = tmp_nodes, black_background = True, cmap = cmap, legend = True, axes_frame = True, 
-                       geometry_size = 25, **base_map_dict, **scheme_dict) 
+    l_cW = {'vScore': 0.25, 'sScore' : 0.35, 'cScore':0.10 , 'pScore': 0.30}
+    l_iW = {'3dvis': 0.50, 'fac': 0.30, 'height': 0.20, 'area': 0.40, '2dvis': 0.00, 'neigh': 0.30 , 'road': 0.30}
     
-    columns = ['Bc', 'Sc', 'Cc', 'Ic']
-    cmap = ci.cmap_from_colors(['yellow', 'orange', 'red'])
-    plot = ci.plot_gdf_grid(gdf = tmp_nodes, columns = columns, black_background = True, cmap = cmap, legend = False, 
-                           geometry_size_factor = 30, **cbar_dict) 
+    buildings_gdf = ci.get_buildings_fromOSM(place, download_method = 'OSMplace', epsg = epsg_susa)
+    buildings_gdf['height'] = np.random.choice([10, 1, 50], buildings_gdf.shape[0]) 
+    print(buildings_gdf.columns)
     
-    cmap = ci.cmap_from_colors(['red', 'blue'])
-    plot_edges = ci.plot_gdf(edges_gdf, column = 'Eb', black_background = True, scheme = 'Fisher_Jenks', cmap = cmap, 
-                             norm = None, legend = False, **cbar_dict)
-    plot_multi = ci.plot_gdfs([edges_gdf, edges_gdf_susa], column = "length", black_background = True, scheme = 'Fisher_Jenks', 
-                              cmap = cmap, **cbar_dict)   
-                              
-    base_map_dict = {'gdf_base_map': edges_gdf_susa, 'base_map_alpha' : 0.4, 'base_map_geometry_size' : 1.1, 'base_map_zorder' : 0}
+    # testing with only 5 nodes, to avoid time issues
+    sight_lines = ci.compute_3d_sight_lines(nodes_gdf_susa.iloc[:5], buildings_gdf, distance_along = 300,
+                                                         distance_min_observer_target = 300)
+    # historical elements                                                    
+    historical = ci.get_historical_buildings_fromOSM(place, download_method = 'OSMplace', epsg = epsg_susa)
+    
+    # scores    
+    buildings_gdf = ci.structural_score(buildings_gdf, buildings_gdf, edges_gdf_susa, advance_vis_expansion_distance = 100, neighbours_radius = 100)   
 
-    barriers_gdf.sort_values(by = 'barrier_type', ascending = False, inplace = True)  
-    colors = ['green', 'red', 'gray', 'blue']
-    if 'secondary_road' in list(barriers_gdf['barrier_type'].unique()):
-        colors.append('gold')
-    cmap = LinearSegmentedColormap.from_list('new_map', colors, N=len(colors)) 
-    plot = ci.plot_gdf(barriers_gdf, column = 'barrier_type', geometry_size = 1.1, legend = True, axes_frame = False, black_background = False, cmap = cmap)                          
-                              
-# def test_landmarks():
+    buildings_gdf = ci.cultural_score(buildings_gdf, historical_elements_gdf = historical, from_OSM = False)
+    buildings_gdf = ci.pragmatic_score(buildings_gdf, research_radius = 200)
+    buildings_gdf, _ = ci.visibility_score(buildings_gdf, sight_lines)
     
-    # epsg = 3003
-    # buildings_gdf = ci.get_buildings_fromOSM(place, download_method = 'OSMplace', epsg = epsg)
-    # buildings_gdf_address = ci.get_buildings_fromOSM(address, download_method = 'distance_from_address', epsg = epsg, distance = 1000)
-    # buildings_gdf_point = ci.get_buildings_fromOSM(location, download_method = 'from_point', epsg = epsg, distance = 1000)
-    # historical = ci.get_historical_buildings_fromOSM(place, download_method = 'OSMplace', epsg = epsg)
+    buildings_gdf = ci.compute_global_scores(buildings_gdf, g_cW, g_iW)
+    buildings_gdf = ci.compute_local_scores(buildings_gdf, l_cW, l_iW, rescaling_radius = 1500)
+
     
+    
+    buildings_shp, _ = ci.get_buildings_fromSHP(input_path, epsg = epsg, height_field = 'height', base_field = 'base', land_use_field = 'land_use')
+       
     # epsg = 25832
     # input_path = 'input/Muenster_buildings.shp'
-    
-    # # buildings_shp, _ = ci.get_buildings_fromSHP(input_path, epsg = epsg, height_field = 'height', base_field = 'base', land_use_field = 'land_use')
-    # # buildings_attributes = ci.attach_attributes(buildings_gdf, attributes_gdf, height_field, base_field, land_use_field)
-    
     # buildings_shp, _ = ci.get_buildings_fromSHP(input_path, epsg = epsg, height_field = 'height', base_field = 'base', land_use_field = 'land_use')
-    # sight_lines = gpd.read_file('input/Muenster_sight_lines.shp')
-    # buildings_shp, _ = ci.visibility_score(buildings_shp, sight_lines = sight_lines)
-    
-    # _, edges_gdf = ci.get_network_fromOSM(place, 'OSMplace', network_type = "drive", epsg = epsg)
-    # buildings_gdf = ci.structural_score(buildings_gdf, buildings_gdf, edges_gdf, max_expansion_distance = 100, distance_along = 50, radius = 100)   
-    
-    # buildings_gdf = ci.cultural_score_from_OSM(buildings_gdf)
-    # buildings_gdf, _ = ci.visibility_score(buildings_gdf)
-    # buildings_gdf['land_use'] = buildings_gdf['land_use_raw']
-    # buildings_gdf = ci.pragmatic_score(buildings_gdf, radius = 200)
-    
-    # g_cW = {'vScore': 0.50, 'sScore' : 0.30, 'cScore': 0.20, 'pScore': 0.10}
-    # g_iW = {'3dvis': 0.50, 'fac': 0.30, 'height': 0.20, 'area': 0.30, '2dvis':0.30, 'neigh': 0.20 , 'road': 0.20}
+    # buildings_attributes = ci.attach_attributes(buildings_gdf, attributes_gdf, height_field, base_field, land_use_field)
+    _ = ci.get_buildings_fromOSM(address, download_method = 'distance_from_address', epsg = epsg_susa, distance = 1000)
+    _ = ci.get_buildings_fromOSM(location, download_method = 'from_point', epsg = epsg_susa, distance = 1000)
 
-    # l_cW = {'vScore': 0.25, 'sScore' : 0.35, 'cScore':0.10 , 'pScore': 0.30}
-    # l_iW = {'3dvis': 0.50, 'fac': 0.30, 'height': 0.20, 'area': 0.40, '2dvis': 0.00, 'neigh': 0.30 , 'road': 0.30}
-    
-    # buildings_gdf = ci.compute_global_scores(buildings_gdf, g_cW, g_iW)
-    # buildings_gdf = ci.compute_local_scores(buildings_gdf, l_cW, l_iW, radius = 1500)
 
-    # cmap = ci.random_colors_list_rgb(nlabels = len(buildings_gdf.land_use.unique()))
-    # plot_buildings = ci.plot_gdf(buildings_gdf, column = 'land_use', black_background = True, fig_size = 15, legend = True)
-    # plot_flat = ci.plot_gdf(buildings_gdf, black_background = True, fig_size = 15)
+
     
     
 
