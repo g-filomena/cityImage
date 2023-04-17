3b7e777f4a1f1cfc542014daf399f66b368b6c6d
Gabriele Filomena <gabriele.filomena@uni-muenster.de>
2023-04-17 12:57:14 +0200

    Update plot.py

--- cityImage/plot.py
+++ cityImage/plot.py
@@ -436,7 +436,6 @@ def plotOn_ax(ax, gdf, column = None, scheme = None, bins = None, classes = 7, n
     c_k = dict(k=classes) if bins is None else dict(bins=bins, k=len(bins))
     scheme = 'User_Defined' if bins is not None else scheme
     
-    parameters = {'ax' : ax, 'column' : column, 'classification_kwds' : c_k, 'scheme' : scheme, 'norm' : norm, 
     parameters = {'ax' : ax, 'column' : column, 'classification_kwds' : c_k, 'scheme' : scheme, 'norm' : norm,
                    'cmap' : cmap, 'categorical' : categorical, 'color' : color, 'alpha' : alpha, 'legend' : legend, 
                    'zorder': zorder}  
