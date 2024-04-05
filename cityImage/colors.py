7c9140905b732a8096f624d213658d771cc7175a
Gabriele Filomena <gfilo@liverpool.ac.uk>
2024-04-05 10:20:09 +0100

    Update colors.py

--- cityImage/colors.py
+++ cityImage/colors.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.colors as mc
 from matplotlib.colors import LinearSegmentedColormap, TwoSlopeNorm
 import colorsys
-import mapclassify, pylab, colorsys
 
 def random_colors_list(nlabels, vmin = 0.8, vmax = 1.0, hsv = False):
     """ 
