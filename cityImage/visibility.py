7f2f4c3523efbac6a25a9cc12840bcda00b16c82
Gabriele Filomena <gfilo@liverpool.ac.uk>
2024-07-24 16:52:34 +0100

    Update visibility.py

--- cityImage/visibility.py
+++ cityImage/visibility.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import geopandas as gpd
 import pyvista as pv
+from tqdm import tqdm
 
 from shapely.geometry import Point, LineString, Polygon, MultiPolygon, MultiLineString
 from shapely.ops import unary_union
@@ -72,7 +73,7 @@ def visibility_polygon2d(building_geometry, obstructions_gdf, obstructions_sinde
     
     return poly_vis.area  
     
-def compute_3d_sight_lines(nodes_gdf, buildings_gdf, distance_along = 200, distance_min_observer_target = 300):
+def compute_3d_sight_lines(nodes_gdf, buildings_gdf, distance_along = 200, distance_min_observer_target = 300, chunk_size = 1000):
     """
     Computes the 3D sight lines between observer points in a Point GeoDataFrame and target buildings, based on a given distance along the buildings' exterior and a 
     minimum distance between observer and target (e.g. lines will not be constructed for observer points and targets whose distance is lower 
@@ -88,20 +89,24 @@ def compute_3d_sight_lines(nodes_gdf, buildings_gdf, distance_along = 200, dista
         The distance along the exterior of the buildings for selecting target points.
     distance_min_observer_target: float 
         The minimum distance between observer and target.
+    chunk_size: int
+        It Computes sight lines in chunks.
     
     Returns:
     ----------
     sight_lines: LineString GeoDataFrame
         The visibile sight lines GeoDataFrame.
     """
-
+    
     nodes_gdf = nodes_gdf.copy()
     buildings_gdf = buildings_gdf.copy()
-    
+
+    # add a 'base' column to the buildings GeoDataFrame with a default value of 0.0, if not provided
     if 'base' not in buildings_gdf.columns:
-        buildings_gdf['base'] = 0.0
+        buildings_gdf["base"] = 0.0
 
     def add_height_to_line(exterior, base, height):
+        # create a new list of Point objects with the z-coordinate set to (height - base)
         coords = exterior.coords
         new_coords = ((x, y, height - base) for x, y in coords)
         return LineString(new_coords)
@@ -110,43 +115,55 @@ def compute_3d_sight_lines(nodes_gdf, buildings_gdf, distance_along = 200, dista
         if isinstance(geometry, Polygon):
             return add_height_to_line(geometry.exterior, base, height)
         elif isinstance(geometry, MultiPolygon):
-            return LineString((pt for poly in geometry.geoms for pt in add_height_to_line(poly.exterior, base, height).coords))
-        
+            return LineString((point for poly in geometry.geoms for point in add_height_to_line(poly.exterior, base, height).coords))
+
+    # create a Series with the height-extended LineString objects
     building_exteriors_roof = buildings_gdf.apply(lambda row: process_geometry_to_linestring(row.geometry, row.base, row.height), axis=1)
+    # create a list with the number of intervals along the exterior LineString to divide the line into
     num_intervals = [max(1, int(exterior.length / distance_along)) for exterior in building_exteriors_roof]
     
+    # create a new column with the list of targets along the exterior LineString
     def interpolate_targets(exterior, num_intervals):
         return (exterior.interpolate(min(exterior.length / 2, distance_along) * i) for i in range(num_intervals))
-    
     buildings_gdf['target'] = [list(interpolate_targets(exterior, intervals)) for exterior, intervals in zip(building_exteriors_roof, num_intervals)]
-    
+
+    # create a new column in the nodes GeoDataFrame with the Point objects representing the observer positions
     nodes_gdf['observer'] = nodes_gdf.apply(lambda row: Point(row.geometry.x, row.geometry.y, row.z), axis=1)
-    tmp_buildings = buildings_gdf.explode('target').reset_index(drop=True)
+    # create a temporary dataframe with building targets
+    tmp_buildings = buildings_gdf.explode('target')
     tmp_nodes = nodes_gdf[['nodeID', 'observer']].copy()
     tmp_buildings = tmp_buildings[['buildingID', 'target']]
 
-    sight_lines_chunks = []
-    tmp_buildings_chunks = np.array_split(tmp_buildings, max(1, len(tmp_buildings) // chunk_size))
+    # create pyvista 3d objects for buildings
+    buildings_gdf['building_3d'] = [polygon_2d_to_3d(geo, base, height) for geo, base, height in zip(buildings_gdf['geometry'], buildings_gdf['base'], buildings_gdf['height'])]
     buildings_sindex = buildings_gdf.sindex
 
-    for chunk in tqdm(tmp_buildings_chunks, desc="Processing sight lines"):
-        sight_lines_chunk = pd.merge(tmp_nodes.assign(key=1), chunk.assign(key=1), on='key').drop('key', axis=1)
-        sight_lines_chunk['distance'] = [p1.distance(p2) for p1, p2 in zip(sight_lines_chunk.observer, sight_lines_chunk.target)]
+    sight_lines_chunks = []
+    tmp_buildings_chunks = np.array_split(tmp_buildings, max(1, len(tmp_buildings) // chunk_size))
+    
+    for buildings_chunk in tqdm(tmp_buildings_chunks, desc="Processing buildings chunks"):
+        # obtain the sight_lines dataframe by means of a cartesian product between GeoDataFrames
+        sight_lines_chunk = pd.merge(tmp_nodes.assign(key=1), buildings_chunk.assign(key=1), on='key').drop('key', axis=1)
+        # calculate the distance between observer and target and filter
+        sight_lines_chunk['distance']= [p1.distance(p2) for p1, p2 in zip(sight_lines_chunk.observer, sight_lines_chunk.target)]
         sight_lines_chunk = sight_lines_chunk[sight_lines_chunk['distance'] >= distance_min_observer_target]
+        # add geometry column with LineString connecting observer and target
         sight_lines_chunk['geometry'] = [LineString([p1, p2]) for p1, p2 in zip(sight_lines_chunk.observer, sight_lines_chunk.target)]
 
-        # Calculate intervisibility right away
+        # extract tuples for starting and target points of the sight lines
         sight_lines_chunk['start'] = [[observer.x, observer.y, observer.z] for observer in sight_lines_chunk.observer]
-        sight_lines_chunk['stop'] = [[target.x, target.y, target.z] for target in sight_lines_chunk.target]
-        sight_lines_chunk['visible'] = sight_lines_chunk.apply(lambda row: intervisibility(row.geometry, row.buildingID, row.start, row.stop, buildings_gdf, buildings_sindex), axis=1)
+        sight_lines_chunk['stop'] =[[target.x, target.y, target.z] for target in sight_lines_chunk.target]
+
+        # check intervisibility and filter out sight_lines that are obstructed
+        sight_lines_chunk['visible'] = sight_lines_chunk.apply(lambda row: intervisibility(row.geometry, row.buildingID, row.start, row.stop, buildings_gdf, buildings_sindex), axis =1)
         sight_lines_chunk = sight_lines_chunk[sight_lines_chunk['visible'] == True]
         sight_lines_chunk.drop(['start', 'stop', 'observer', 'target', 'visible'], axis=1, inplace=True)
         sight_lines_chunks.append(sight_lines_chunk)
-
+    
     sight_lines = pd.concat(sight_lines_chunks, ignore_index=True)
     sight_lines.reset_index(drop=True, inplace=True)
     sight_lines = sight_lines.set_geometry('geometry').set_crs(buildings_gdf.crs)
-    
+
     return sight_lines
    
 def intervisibility(line2d, buildingID, start, stop, obstructions_gdf, obstructions_sindex):
@@ -186,4 +203,4 @@ def intervisibility(line2d, buildingID, start, stop, obstructions_gdf, obstructi
         if len(intersections) > 0:
             return False
             
-    return visible 
+    return visible
