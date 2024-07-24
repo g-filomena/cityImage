0ba7ba8984d7a8de8e283aa11dd2ac96a52b730d
Gabriele Filomena <gfilo@liverpool.ac.uk>
2024-07-24 16:49:39 +0100

    Update __init__.py

--- cityImage/__init__.py
+++ cityImage/__init__.py
@@ -1,8 +1,7 @@
 ################################################################################
 # Module: __init__.py
-# Description: cityImage - a package for Street Network Analysis and 
-#               landmarks extractions and integration in the Street Network.
-# License: MIT, see full license in LICENSE.txt
+# Description: cityImage - a package for capturing the Image of the City with street network and GIS-based analysis.
+# License: GNU General Public License v3.0, see full license in LICENSE.txt
 ################################################################################
 
 from .load import*
@@ -19,4 +18,4 @@ from .regions import *
 from .utilities import *
 from .visibility import *
 
-__version__ = '0.14'
+__version__ = '1.21'
