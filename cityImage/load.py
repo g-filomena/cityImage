92019265f1d6064984c16090576699fbe0185ad8
Gabriele Filomena <gfilo@liverpool.ac.uk>
2025-07-28 15:22:16 +0200

    Update load.py

--- cityImage/load.py
+++ cityImage/load.py
@@ -17,7 +17,7 @@ from .utilities import fix_multiparts_LineString_gdf, downloader
 pd.set_option("display.precision", 3)
 pd.options.mode.chained_assignment = None
    
-def get_network_fromOSM(place, download_method, network_type = "all", epsg = None, distance = 500.0): 
+def get_network_fromOSM(place, download_method, network_type = "all", crs = None, distance = 500.0): 
     """
     The function downloads and creates a simplified OSMNx graph for a selected area's street network.
     Afterwards, GeoDataFrames for nodes and edges are created, assigning new nodeID and edgeID identifiers.
@@ -35,8 +35,8 @@ def get_network_fromOSM(place, download_method, network_type = "all", epsg = Non
         unprojected latitude-longitude degrees (EPSG:4326).
     network_type: str {"walk", "bike", "drive", "drive_service", "all", "all_private", "none"}
         It indicates type of street or other network to extract - from OSMNx paramaters.
-    epsg: int
-        Epsg of the area considered; if None OSMNx is used for the projection.
+    crs : str or pyproj.CRS. 
+        Coordinate Reference System for the GeoDataFrame. Pass as a string (e.g., 'EPSG:32633') or a pyproj.CRS object.
     distance: float
         It is used only if download_method == "distance_from_address" or "distance_from_point".
         
@@ -46,7 +46,7 @@ def get_network_fromOSM(place, download_method, network_type = "all", epsg = Non
         the junction and street segments GeoDataFrames.
     """
    
-    G = downloader(place = place, download_method = download_method, distance = distance, downloading_graph = True, network_type = network_type)
+    G = downloader(OSMplace = OSMplace, download_method = download_method, distance = distance, downloading_graph = True, network_type = network_type)
     
     # fix list of osmid assigned to same edges
     for i, item in enumerate(G.edges()):
@@ -63,7 +63,6 @@ def get_network_fromOSM(place, download_method, network_type = "all", epsg = Non
                
     # columns to keep (u and v represent "from" and "to" node)
     nodes_gdf = nodes_gdf[["nodeID","x","y","geometry"]]
-    
     to_keep = ["edgeID", "u", "v", "key", "geometry", "length", "name", "highway", "oneway"]
     
     for column in ["lanes", "bridge", "tunnel"]:
@@ -74,7 +73,7 @@ def get_network_fromOSM(place, download_method, network_type = "all", epsg = Non
     
     edges_gdf = edges_gdf[to_keep]
     edges_gdf = _resolve_lists(edges_gdf)
-    nodes_gdf, edges_gdf = _project_gdfs(nodes_gdf, edges_gdf, epsg)
+    nodes_gdf, edges_gdf = _project_gdfs(nodes_gdf, edges_gdf, crs)
     nodes_gdf["x"], nodes_gdf["y"] = list(zip(*[(geometry.coords[0][0], geometry.coords[0][1]) for geometry in nodes_gdf.geometry]))
     
     if len(nodes_gdf.geometry.iloc[0].coords) > 2:
@@ -86,7 +85,7 @@ def get_network_fromOSM(place, download_method, network_type = "all", epsg = Non
 
     return nodes_gdf, edges_gdf
     
-def get_pedestrian_network_fromOSM(place, download_method, epsg = None, distance = 500.0):
+def get_pedestrian_network_fromOSM(OSMplace, download_method, crs = None, distance = 500.0):
     """
     The function downloads and creates a simplified OSMNx graph for a selected area's street network. Compare to get_network_fromOSM, 
     this is more precise in terms of pedestrian paths and it includes the column "lit", and anything the refers to "sidewalk".
@@ -94,7 +93,7 @@ def get_pedestrian_network_fromOSM(place, download_method, epsg = None, distance
         
     Parameters
     ----------
-    place: str, tuple
+    OSMplace: str, tuple
         Name of cities or areas in OSM: 
         - when using "distance_from_point" please provide a (lat, lon) tuple to create the bounding box around it; 
         - when using "distance_from_address" provide an existing OSM address; 
@@ -103,8 +102,8 @@ def get_pedestrian_network_fromOSM(place, download_method, epsg = None, distance
     download_method: str, {"distance_from_address", "distance_from_point", "OSMpolygon", "OSMplace", "polygon"}
         It indicates the method that should be used for downloading the data. When 'polygon' the shape to get network data within coordinates should be in
         unprojected latitude-longitude degrees (EPSG:4326).
-    epsg: int
-        Epsg of the area considered; if None OSMNx is used for the projection.
+    crs : str or pyproj.CRS. 
+        Coordinate Reference System for the GeoDataFrame. Pass as a string (e.g., 'EPSG:32633') or a pyproj.CRS object.
     distance: float
         It is used only if download_method == "distance_from_address" or "distance_from_point".
         
@@ -118,7 +117,7 @@ def get_pedestrian_network_fromOSM(place, download_method, epsg = None, distance
     tags = {"highway": True}
     
     # Retrieve geometries
-    gdf = downloader(place = place, download_method = download_method, tags = tags, distance = distance)
+    gdf = downloader(OSMplace = OSMplace, download_method = download_method, tags = tags, distance = distance)
        
     # Make sure columns exist (otherwise you'll get a KeyError)
     for col in ["area", "foot", "service", "sidewalk",
@@ -157,13 +156,30 @@ def get_pedestrian_network_fromOSM(place, download_method, epsg = None, distance
     gdf = gdf.drop(["element_type", "osmid"], axis = 1, errors = 'ignore')
     
     nodes_gdf, edges_gdf = get_network_fromGDF(gdf, epsg, other_columns = ["name", "highway", "lit"])
-    edges_gdf = _resolve_lists(edges_gdf)
+    edges_gdf = _resolve_list_columns(edges_gdf)
     
     return nodes_gdf, edges_gdf
 
-def _resolve_lists(edges_gdf):
+def _resolve_list_columns(edges_gdf):
 
-    # Resolving lists
+    """
+    Resolves list-type values in specified columns of an edge GeoDataFrame.
+
+    For the columns 'highway', 'name', and 'oneway', if a cell contains a list, only the first element is kept.
+    For the columns 'lanes', 'bridge', and 'tunnel', if a cell contains a list, the maximum value is kept.
+    For 'bridge' and 'tunnel', values are then binarized: 1 if present, 0 otherwise (or if NaN/False).
+
+    Parameters
+    ----------
+    edges_gdf : GeoDataFrame
+        Edge GeoDataFrame whose specified columns may contain list-type values.
+
+    Returns
+    -------
+    edges_gdf : GeoDataFrame
+        Modified GeoDataFrame with resolved columns.
+    """
+    
     for column in ["highway", "name", "oneway"]:
         if column in edges_gdf.columns:
             edges_gdf[column] = [x[0] if isinstance(x, list) else x for x in edges_gdf[column]]
@@ -176,77 +192,66 @@ def _resolve_lists(edges_gdf):
                 
     return edges_gdf
 
-def _project_gdfs(nodes_gdf, edges_gdf, epsg):
-    
-    # finalising geodataframes
-    if epsg is None: 
-        nodes_gdf, edges_gdf = ox.projection.project_gdf(nodes_gdf), ox.projection.project_gdf(edges_gdf)
-    else: 
-        crs = 'EPSG:' + str(epsg)
-        nodes_gdf, edges_gdf = nodes_gdf.to_crs(crs), edges_gdf.to_crs(crs)
-        
+def _project_gdfs(nodes_gdf, edges_gdf, crs):
+    """
+    Projects nodes and edges GeoDataFrames to the specified coordinate reference system (CRS).
+
+    Parameters
+    ----------
+    nodes_gdf : GeoDataFrame
+        GeoDataFrame of network nodes.
+    edges_gdf : GeoDataFrame
+        GeoDataFrame of network edges.
+    crs : str or pyproj.CRS. 
+        Coordinate Reference System for the GeoDataFrame. Pass as a string (e.g., 'EPSG:32633') or a pyproj.CRS object.
+
+    Returns
+    -------
+    nodes_gdf : GeoDataFrame
+        Projected nodes GeoDataFrame.
+    edges_gdf : GeoDataFrame
+        Projected edges GeoDataFrame.
+    """
+    nodes_gdf, edges_gdf = nodes_gdf.to_crs(crs), edges_gdf.to_crs(crs)
     return nodes_gdf, edges_gdf
     
-def get_network_fromFile(path, epsg, dict_columns = {}, other_columns = []):
+def get_network_fromGDF(edges_gdf, crs, dict_columns={}, other_columns=[]):
     """
-    The function loads a vector lines from a specified directory, along with the epsg coordinate code.
-    It creates two GeoDataFrame, one for street junctions (nodes) and one for street segments (edges).
-    The GeoDataFrames are built assuming a planar undirected graph. 
-   
+    Constructs node and edge GeoDataFrames from a LineString GeoDataFrame representing a street network.
+
+    The function processes a GeoDataFrame of vector line features (edges), along with the coordinate reference system (CRS).
+    It builds two GeoDataFrames: one for street junctions (nodes) and one for street segments (edges).
+    The network is assumed to be planar and undirected.
+
     Parameters
     ----------
-    path: str
-        The local path where the file is stored, including its extention (".shp", ..).
-    epsg: int
-        Epsg of the area considered.
-    dict_columns: dict
-        It should be structured as: {"highway": "roadType_field",  "oneway": "direction_field", "lanes": "nr. lanes", "maxspeed": "speed_field", "name": "name_field"}.
-        Replace the items with the field names in the input data (if the relative attributes are relevant and existing).
-    other_columns: list
-        Other columns to be preserved in the edges_gdf GeoDataFrame.
-    
+    edges_gdf : GeoDataFrame
+        GeoDataFrame containing the street segments as LineString geometries.
+    crs : str or pyproj.CRS
+        Coordinate Reference System for the output GeoDataFrames. Pass as a string (e.g., 'EPSG:32633') or a pyproj.CRS object.
+    dict_columns : dict, optional
+        Dictionary mapping standard network attribute names to column names in the input data.
+        For example:
+            {
+                "highway": "roadType_field",
+                "oneway": "direction_field",
+                "lanes": "nr_lanes_field",
+                "maxspeed": "speed_field",
+                "name": "name_field"
+            }
+        Only include attributes that are relevant and available in your input.
+    other_columns : list, optional
+        List of additional column names in the input GeoDataFrame to preserve in the output `edges_gdf`.
+
     Returns
     -------
-    nodes_gdf, edges_gdf: tuple
-        The junction and street segment GeoDataFrames.
+    nodes_gdf : GeoDataFrame
+        GeoDataFrame of street junctions (nodes), with unique node IDs and geometries.
+    edges_gdf : GeoDataFrame
+        GeoDataFrame of street segments (edges), with all relevant and requested attributes.
     """
-    # try reading street network from directory
-    crs = 'EPSG:' + str(epsg)
-    edges_gdf = gpd.read_file(path)
-    
-    return get_network_fromGDF(edges_gdf, epsg, dict_columns = dict_columns, other_columns = other_columns)
-    
-    
-def get_network_fromGDF(edges_gdf, epsg, dict_columns = {}, other_columns = []):
-    """
-    The function loads a vector lines shapefile from a given LineString GeoDataFrame, along with the epsg coordinate code.
-    It creates two GeoDataFrame, one for street junctions (nodes) and one for street segments (edges).
-    The GeoDataFrames are built assuming a planar undirected graph. 
-     
-    Parameters
-    ----------
-    path: str
-        the local path where the .shp file is stored.
-    epsg: int
-        Epsg of the area considered .
-    dict_columns: dict
-        It should be structured as: {"highway": "roadType_field",  "oneway": "direction_field", "lanes": "nr. lanes", "maxspeed": "speed_field", "name": "name_field"}.
-        Replace the items with the field names in the input data (if the relative attributes are relevant and existing).
-    other_columns: list
-        Other columns to be preserved in the edges_gdf GeoDataFrame.
-    
-    Returns
-    -------
-    nodes_gdf, edges_gdf: tuple
-        The junction and street segment GeoDataFrames.
-    """
-    # try reading street network from directory
-    crs = 'EPSG:' + str(epsg)
-    try:
-        edges_gdf = edges_gdf.to_crs(crs)
-    except:
-        edges_gdf.crs = crs
-       
+
+    edges_gdf = edges_gdf.to_crs(crs)       
     edges_gdf["key"] = 0
     
     # creating the dataframes
@@ -279,7 +284,34 @@ def get_network_fromGDF(edges_gdf, epsg, dict_columns = {}, other_columns = []):
     nodes_gdf = nodes_gdf[nodes_gdf.nodeID.isin(np.unique(edges_gdf[['u', 'v']].values))]
 
     return nodes_gdf, edges_gdf      
-
+    
+def get_network_fromFile(input_path, crs, dict_columns = {}, other_columns = []):
+    """
+    The function loads a vector lines from a specified directory, along with the epsg coordinate code.
+    It creates two GeoDataFrame, one for street junctions (nodes) and one for street segments (edges).
+    The GeoDataFrames are built assuming a planar undirected graph. 
+   
+    Parameters
+    ----------
+    input_path: str
+        The local path where the file is stored, including its extention (".shp", ..).
+    crs : str or pyproj.CRS. 
+        Coordinate Reference System for the GeoDataFrame. Pass as a string (e.g., 'EPSG:32633') or a pyproj.CRS object.
+    dict_columns: dict
+        It should be structured as: {"highway": "roadType_field",  "oneway": "direction_field", "lanes": "nr. lanes", "maxspeed": "speed_field", "name": "name_field"}.
+        Replace the items with the field names in the input data (if the relative attributes are relevant and existing).
+    other_columns: list
+        Other columns to be preserved in the edges_gdf GeoDataFrame.
+    
+    Returns
+    -------
+    nodes_gdf, edges_gdf: tuple
+        The junction and street segment GeoDataFrames.
+    """
+    edges_gdf = gpd.read_file(input_path)
+    
+    return get_network_fromGDF(edges_gdf, crs, dict_columns = dict_columns, other_columns = other_columns)
+    
 def obtain_nodes_gdf(edges_gdf, crs):
     """
     It obtains the nodes GeoDataFrame from the unique coordinates pairs in the edges_gdf GeoDataFrame.
@@ -288,8 +320,8 @@ def obtain_nodes_gdf(edges_gdf, crs):
     ----------
     edges_gdf: LineString GeoDataFrame
         Street segments GeoDataFrame.
-    crs: str
-        Coordinate reference system of the area considered .
+    crs : str or pyproj.CRS. 
+        Coordinate Reference System for the GeoDataFrame. Pass as a string (e.g., 'EPSG:32633') or a pyproj.CRS object.
     Returns
     -------
     nodes_gdf: Point GeoDataFrame
