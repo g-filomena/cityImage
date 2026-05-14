1ee7949b7b68d2bd0f2bd1e3ef06e641ae3086e1
Gabriele Filomena <gfilo@liverpool.ac.uk>
2026-05-14 14:30:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open("README.md", "r", encoding='utf-8') as readme_file:
-    readme = readme_file.read()
-
-requirements = ["dask>=2025.7.0",  "igraph>=0.11.9", "numba>=0.61.2", "osmnx>=2.0.5", "python-louvain>=0.16", "pyvista>=0.45.3", "tqdm>=4.65.0"]
-    
-setup(
-    name="cityImage",
-    description="A package for studying urban form and obtaining the computational Image of the City",
-    long_description = readme,
-    long_description_content_type="text/markdown",
-    packages=find_packages(),
-    version="1.2.2",
-    author="Gabriele Filomena",
-    author_email="gabriele.filomena@liverpool.ac.uk",
-    install_requires=requirements,
-    keywords=["urban Form Analysis","Computational Image of the City", "Kevin Lynch", "cognitive maps"],
-    )
+if __name__ == "__main__":
+    setup()
