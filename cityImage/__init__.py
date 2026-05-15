839ef5634f77b8fe170d6e5e705d50b67f41e841
Filomena <gfilo@liverpool.ac.uk>
2026-05-15 14:39:27 +0100

    Update __init__.py

--- cityImage/__init__.py
+++ cityImage/__init__.py
@@ -6,25 +6,25 @@
 
 from .angles import *
 from .barriers import *
-from .colors import*
+from .buildings_height import *
+from .buildings_landmarks import *
+from .buildings_load import *
+from .buildings_visibility import *
+from .colors import *
 from .graph import *
 from .graph_centrality import *
 from .graph_clean import *
-from .graph_load import *
 from .graph_consolidate import *
+from .graph_load import *
 from .graph_topology import *
-from .buildings_height import *
-from .buildings_landmarks import *
-from .buildings_load import*
-from .buildings_visibility import *
-from .land_use_assign import*
-from .land_use_classify import*
-from .land_use_derive import*
-from .land_use_tags import*
-from .land_use_utils import*
+from .land_use_assign import *
+from .land_use_classify import *
+from .land_use_derive import *
+from .land_use_sparse import *
+from .land_use_tags import *
+from .land_use_utils import *
 from .plot import *
 from .regions import *
 from .utilities import *
 
-from .land_use_sparse import*
 __version__ = '1.2.3'
